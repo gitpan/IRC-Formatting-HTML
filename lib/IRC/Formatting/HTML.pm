f965cff16d54aa9941896808ab60681c7f2e96b3
leedo <leedo@cpan.org>
2009-12-23 05:32:33 +0000

    Import of LEEDO/IRC-Formatting-HTML-0.10 from CPAN.
    gitpan-cpan-distribution: IRC-Formatting-HTML
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         LEEDO/IRC-Formatting-HTML-0.10.tar.gz
    gitpan-cpan-author:       LEEDO
    gitpan-cpan-maturity:     released

--- lib/IRC/Formatting/HTML.pm
+++ lib/IRC/Formatting/HTML.pm
@@ -13,11 +13,11 @@ IRC::Formatting::HTML - Convert raw IRC formatting to HTML
 
 =head1 VERSION
 
-Version 0.09
+Version 0.10
 
 =cut
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 my $BOLD      = "\002",
 my $COLOR     = "\003";
